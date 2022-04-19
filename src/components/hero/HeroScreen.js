59b739601c3f00b4c8f2db5ba30dddf1a61f904c
aNd12326 <andersonventosilla0@gmail.com>
2022-04-19 09:11:37 -0500

    180 finished

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,22 +1,61 @@
-import { useParams, Navigate } from "react-router-dom"
+import { useState } from "react";
+import { useParams, Navigate, useNavigate } from "react-router-dom"
 import { getHeroById } from "../../selectores/getHeroById";
 
 export const HeroScreen = () => {
 
+  const [counter,setCounter] = useState(0)
+
   const {heroeId} = useParams();
 
+  const navigate = useNavigate()
+
   const hero = getHeroById(heroeId);
+
+  const handleReturn = () => {
+    navigate(-1)
+  }
 
   if(!hero) {
     return <Navigate to='/' />
   }
 
+  const {
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters
+  } = hero
+
+  const imagePath = `/assets/${id}.jpg`;
+
   return (
-    <div>
-        <h1>HeroScreen</h1>
-        <p>
-          {hero.superhero}
-        </p>
+    <div className="row mt-5">
+        <div className="col-4">
+          <img src={imagePath} alt={superhero}
+            className='img-thumbnail'
+          />
+        </div>
+        <div className="col-8">
+          <h3>{hero.superhero}</h3>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item"><b>Alter ego:</b>{alter_ego}</li>
+            <li className="list-group-item"><b>publisher:</b>{publisher}</li>
+            <li className="list-group-item"><b>first appearance:</b>{first_appearance}</li>
+          </ul>
+
+          <h5 className="mt-3">characters</h5>
+          <p>{characters}</p>
+
+          <button
+            className="btn btn-outline-info"
+            onClick={handleReturn}
+          >
+            Regresar
+          </button>
+        </div>
     </div>
   )
 }
--- src/selectores/getHeroById.js
+++ src/selectores/getHeroById.js
@@ -2,5 +2,6 @@ import { heroes } from "../data/heroes"
 
 
 export const getHeroById = ( id = '') => {
+    console.log('getHerobyid called')
     return heroes.find(hero => hero.id === id)
 }
